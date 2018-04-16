def513cb97d0264973fa4fa9ce1116bfdbde4338
danielchisica <daniel.chisica@hotmail.com>
2018-04-16 00:46:34 -0500

    Change .isDone attribute and put the correct check function

--- Logic/Task.js
+++ Logic/Task.js
@@ -1,10 +1,10 @@
 /**
-* The constructor of Task Objects
-*
-* @param {String} Name of the task
-* @param {Date} Date when the task is attached
-* @param {Boolean} True if the task is finished
-*/
+ * The constructor of Task Objects
+ *
+ * @param {String} Name of the task
+ * @param {Date} Date when the task is attached
+ * @param {Boolean} True if the task is finished
+ */
 function Task(name, date, isDone) {
     this.name = name;
     this.date = date;
@@ -13,13 +13,28 @@ function Task(name, date, isDone) {
 
 var taskList = [];
 var currentTime;
+var deleteButtons = [];
+var Checkboxes;
 
 var btnTask = document.getElementById("add-task");
 var list = document.getElementById("list").getElementsByTagName("ul")[0];
+var deleteTask = function () {
+    console.log(this.getAttribute("id"));
+    taskList.splice(this.getAttribute("id"), 1);
+    repaint();
+};
+
+var checkit= function(){
+    var p=this.getAttribute("id");
+    console.log(" "+p);
+   !taskList[this.getAttribute("id")].isDone;
+    repaint();
+};
+
 
 /*
-* Sets a function that generates a new task each time when is pressed the Add button, and assign it to the tasklist
-*/
+ * Sets a function that generates a new task each time when is pressed the Add button, and assign it to the tasklist
+ */
 btnTask.onclick = function () {
     var d = new Date();
     currentTime = d.toString();
@@ -33,12 +48,15 @@ btnTask.onclick = function () {
     repaint();
 };
 
+
+
+
 /**
-* Sets a function with the purpose of rebuild the tasklist in the DOM
-*/
+ * Sets a function with the purpose of rebuild the tasklist in the DOM
+ */
 function repaint() {
     while (list.firstChild) {
-    list.removeChild(list.firstChild);
+        list.removeChild(list.firstChild);
     }
 
     for (var i = 0; i < taskList.length; i++) {
@@ -50,23 +68,26 @@ function repaint() {
         var deleteButton = document.createElement("button");
 
         newCheckbox.setAttribute("type", "checkbox");
-        newCheckbox.setAttribute("input", taskList[i].isDone);
-        newCheckbox.setAttribute("class","check");
+        newCheckbox.setAttribute("input", true);
+        newCheckbox.setAttribute("class", "check");
+        newCheckbox.setAttribute("id",i);
 
 
 
+        deleteButton.setAttribute("class", "delete");
+        deleteButton.setAttribute("id",i);
 
-        deleteButton.setAttribute("class","delete");
 
-        date2.setAttribute("class","date-space");
+
+        date2.setAttribute("class", "date-space");
         date2.setAttribute("type", "text");
         date2.setAttribute("value", taskList[i].date);
         newTask.setAttribute("type", "text");
         newTask.setAttribute("value", taskList[i].name);
-        newTask.setAttribute("class","task-space");
+        newTask.setAttribute("class", "task-space");
 
-        document.getElementsByClassName("date-space").disabled=true;
-        document.getElementsByClassName("task-space").disabled=true;
+        document.getElementsByClassName("date-space").disabled = true;
+        document.getElementsByClassName("task-space").disabled = true;
 
 
         newLi.appendChild(newCheckbox);
@@ -74,24 +95,23 @@ function repaint() {
         newLi.appendChild(newTask);
         newLi.appendChild(deleteButton);
 
+
+
         list.appendChild(newLi);
-    };
 
-    var deleteButtons= document.getElementsByClassName("delete");
-    var Checkboxes= document.getElementsByClassName("check");
 
-    for (var i = 0; i < deleteButtons.length; i++) {
 
-    /*
-    * Assigns a function to each deletebutton, those functions will delete the assigned task to its button
-    */
-    deleteButtons[i].onclick = function(){
-        taskList.splice(i,1);
-        repaint();
-        };
+        deleteButtons = document.getElementsByClassName("delete");
+        Checkboxes = document.getElementsByClassName("check");
+
+        deleteButtons[i].onclick = deleteTask;
+        Checkboxes[i].onclick=checkit;
     }
-
 };
 
- document.getElementsByClassName("date-space").disabled=true;
-document.getElementsByClassName("task-space").disabled=true;
+
+
+
+
+document.getElementsByClassName("date-space").disabled = true;
+document.getElementsByClassName("task-space").disabled = true;
